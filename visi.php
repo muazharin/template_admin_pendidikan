0d707b9f87b0979f98cdeee0c070d9ed5ef243b1
muazharin <alfanmuazharin@gmail.com>
2019-05-25 01:38:07 +0700

    halaman visi

---
+++ visi.php
@@ -0,0 +1,109 @@
+<?php include("header.php");?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Home
+        <small>Menyusun Visi</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Visi</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box box-primary">
+            <div class="box-header">
+              <h3 class="box-title">Menyusun Visi OPD</h3>
+              <hr>
+              <table id="">
+                <tbody>
+                <tr>
+                  <td width="15%">Urusan </td>
+                  <td width="75%">: Urusan Wajib Pelayanan Dasar</td>
+                </tr>
+                <tr>
+                  <td width="15%">Bidang </td>
+                  <td width="75%">: Pendidikan</td>
+                </tr>
+                <tr>
+                  <td width="15%">Unit </td>
+                  <td width="75%">: Dinas Pendidikan dan Kebudayaan</td>
+                </tr>
+                <tr>
+                  <td width="15%">Sub </td>
+                  <td width="75%">: Dinas Pendidikan dan Kebudayaan</td>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-header -->
+            <div align="center">
+              <hr>
+              <h1>Visi OPD</h1>
+              <hr>
+            </div>
+            <button type="button" class="btn bg-maroon margin"><i class="fa fa-fw fa-file-pdf-o"></i> Pdf</button>
+            <button type="button" class="btn bg-olive margin"><i class="fa fa-fw fa-file-excel-o"></i> Excel</button>
+            <div class="box-body">
+            <table id="" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Visi</th>
+                  <th>Aksi</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Trident</td>
+                  <td> 4</td>
+                  <td><button type="button" class="btn bg-orange margin"><i class="fa fa-fw fa-edit"></i></button></td>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+            <div align="center">
+                <hr>
+                <h1>Visi Penjelasan OPD</h1>
+                <hr>
+              </div>
+              <button type="button" class="btn bg-maroon margin"><i class="fa fa-fw fa-file-pdf-o"></i> Pdf</button>
+              <button type="button" class="btn bg-olive margin"><i class="fa fa-fw fa-file-excel-o"></i> Excel</button>
+              <button type="button" class="btn bg-navy margin" data-toggle="modal" data-target="#modal-visi"><i class="fa fa-fw fa-plus-square-o"></i> Tambah</button>
+              <div class="box-body">
+                <table id="" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Visi</th>
+                    <th>Aksi</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>Trident</td>
+                    <td> 4</td>
+                    <td><button type="button" class="btn bg-orange margin"><i class="fa fa-fw fa-edit"></i></button></td>
+                  </tr>
+                  </tfoot>
+                </table>
+              </div>
+              <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+<?php include("footer.php");?>
